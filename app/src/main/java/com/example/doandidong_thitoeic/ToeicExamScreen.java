de37f34a79edd6562fa0d9db2ed0254edbf58c47
Sáng Huỳnh TDC <sanghuynhtdc123@users.noreply.github.com>
2020-07-11 10:43:40 +0700

    Đây là phần  nội dung bắt đầu  của đề tài thi toeic!

---
+++ app/src/main/java/com/example/doandidong_thitoeic/ToeicExamScreen.java
@@ -0,0 +1,14 @@
+package com.example.doandidong_thitoeic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToeicExamScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.toeic_exam_screen_layout);
+    }
+}
